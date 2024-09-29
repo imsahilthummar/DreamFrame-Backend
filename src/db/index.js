2cc10c5b95dbba77ef968e26ed48ee8d9bee0eb2
Sahil Thummar <sahilthummar2652@gmail.com>
2024-09-29 20:01:20 +0530

    controllers added

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGODB_URL}`);
     console.log("MongoDB connected !!")
   } catch (error) {
     console.log("MONGODB connection error", error);
